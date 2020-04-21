4fca8a63fdd4600a8f5e8c0f4ac127cfb86715cb
Deep Singh <sinriat@gmail.com>
2020-04-22 00:16:37 +0200

    feat: invite new players to a team; leave team (only implemented if you're not the leader, if leader, then delete the whole team and their memebers)

--- src/io/riat/CTF/Commands/InviteTeam.java
+++ src/io/riat/CTF/Commands/InviteTeam.java
@@ -6,6 +6,9 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class InviteTeam implements CommandExecutor {
 
@@ -20,8 +23,93 @@ public class InviteTeam implements CommandExecutor {
         if (commandSender instanceof Player) {
             Player player = (Player) commandSender;
 
+            Integer team = getPlayerTeam(player);
 
+            // Check if current player is in a team
+            if (team == null) {
+                player.sendMessage("[CTF] You're not in a team?! Create a team first (hint: /createteam <COLOR>");
+                return false;
+            }
 
+            // Check if invited player is on the server (if not error)
+            if (strings.length == 0) {
+                return false;
+            }
+
+            String newPlayer = strings[0];
+
+            if (!addNewPlayerToTeam(team, newPlayer)) {
+                player.sendMessage(
+                    "[CTF] The player you're trying to invite hasn't connected to the server before or is in a team!"
+                );
+
+                return false;
+            }
+
+            // Update the invited player team
+            player.sendMessage(String.format("[CTF] %S has now been added to your team!", newPlayer));
+        }
+
+        return true;
+    }
+
+    /**
+     * Check if user is in a team
+     *
+     * @param player current player instance.
+     * @return in the team or not.
+     */
+    private Integer getPlayerTeam(Player player) {
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM users WHERE uuid = ?");
+            statement.setString(1, player.getUniqueId().toString());
+            ResultSet userResult = statement.executeQuery();
+
+            if (userResult.next()) {
+                Integer team = (Integer) userResult.getObject("team");
+
+                // User is already in a team!
+                return team;
+            }
+
+            // This should never happen, because users should be inserted into the database on join.
+            return null;
+
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
+
+        return null;
+    }
+
+    private boolean addNewPlayerToTeam(Integer team, String player) {
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM users WHERE name = ?");
+            statement.setString(1, player);
+            ResultSet userResult = statement.executeQuery();
+
+            if (userResult.next()) {
+                int userPK = userResult.getInt(1);  // New player ID
+                Integer getTeam = (Integer) userResult.getObject("team");
+
+                System.out.println(getTeam + " " + userPK);
+
+                if (getTeam != null) {
+                    return false;
+                }
+
+                PreparedStatement updatePlayerStatement = connection.prepareStatement(
+                        "UPDATE users SET team = ? WHERE id = ?");
+
+                updatePlayerStatement.setInt(1, team);
+                updatePlayerStatement.setInt(2, userPK);
+
+                if (updatePlayerStatement.executeUpdate() > 0) {
+                    return true;
+                }
+            }
+        } catch (SQLException e) {
+            e.getStackTrace();
         }
 
         return false;
--- src/io/riat/CTF/Commands/LeaveTeam.java
+++ src/io/riat/CTF/Commands/LeaveTeam.java
@@ -3,8 +3,12 @@ package io.riat.CTF.Commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class LeaveTeam implements CommandExecutor {
 
@@ -16,6 +20,78 @@ public class LeaveTeam implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        return false;
+        if (commandSender instanceof Player) {
+            Player player = (Player) commandSender;
+
+            // Check if in team.
+            Integer team = isPlayerInTeam(player);
+
+            if (team == null) {
+                player.sendMessage("[CTF] You can't leave a team, if your not in a team!");
+                return false;
+            }
+
+            leaveTeam(team, player);
+
+        }
+
+
+        return true;
     }
+
+    private Integer isPlayerInTeam(Player player) {
+
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM users WHERE uuid = ?");
+            statement.setString(1, player.getUniqueId().toString());
+
+            ResultSet result = statement.executeQuery();
+
+            if (result.next()) {
+                Integer team = (Integer) result.getObject("team");
+
+                return team;
+            }
+
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
+
+        return null;
+    }
+
+    private void leaveTeam(Integer team, Player player) {
+
+        try {
+            PreparedStatement teamStatement = connection.prepareStatement("SELECT * FROM teams WHERE id = ?");
+            teamStatement.setInt(1, team);
+            ResultSet teamResult = teamStatement.executeQuery();
+
+            if (teamResult.next()) {
+                String leader = teamResult.getString("leader");
+                String color = teamResult.getString("color");
+
+                if (player.getUniqueId().toString().equals(leader)) {
+                    // if leader, kick everyone out.
+
+                } else {
+                    // if not leader, just leave the team.
+                    PreparedStatement playerStatement = connection.prepareStatement(
+                            "UPDATE users SET team = ? WHERE uuid = ?");
+                    playerStatement.setObject(1, null);
+                    playerStatement.setString(2, player.getUniqueId().toString());
+
+                    if (playerStatement.executeUpdate() > 0) {
+                        player.sendMessage("[CTF] You have left the " + color + " team, farewell!");
+                    }
+                }
+
+
+            }
+
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
+    }
+
 }
