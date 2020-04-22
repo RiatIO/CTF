ced644fc5d783a1c4a21f90a6477eb0ed46e76bd
Deep Singh <sinriat@gmail.com>
2020-04-22 23:14:54 +0200

    feat: if leader leaves, kick all the players and remove the team

--- src/io/riat/CTF/Commands/LeaveTeam.java
+++ src/io/riat/CTF/Commands/LeaveTeam.java
@@ -70,28 +70,68 @@ public class LeaveTeam implements CommandExecutor {
             if (teamResult.next()) {
                 String leader = teamResult.getString("leader");
                 String color = teamResult.getString("color");
+                int teamID = teamResult.getInt("id");
 
                 if (player.getUniqueId().toString().equals(leader)) {
                     // if leader, kick everyone out.
+                    PreparedStatement playerStatement = connection.prepareStatement(
+                            "SELECT * FROM users WHERE team = ?"
+                    );
+                    playerStatement.setInt(1, team);
+                    ResultSet playerResult = playerStatement.executeQuery();
+                    System.out.println("team " + team);
+                    System.out.println("player res " + playerResult.getFetchSize());
 
+                    while (playerResult.next()) {
+                        String playerUUID = playerResult.getString("uuid");
+                        String playerName = playerResult.getString("name");
+                        if (!removePlayerFromTeam(playerUUID)) {
+                            player.sendMessage("[CTF] Player " + playerName + " had troubles leaving, wops?");
+                        }
+                    }
+
+                    // Delete the team too
+                    PreparedStatement deleteTeamStatement = connection.prepareStatement(
+                            "DELETE FROM teams WHERE id = ?"
+                    );
+                    deleteTeamStatement.setInt(1, teamID);
+
+                    if (deleteTeamStatement.executeUpdate() > 0) {
+                        player.sendMessage(
+                                "[CTF] You took down the whole team " + color + " and its players, hope you're happy"
+                        );
+                    }
                 } else {
                     // if not leader, just leave the team.
-                    PreparedStatement playerStatement = connection.prepareStatement(
-                            "UPDATE users SET team = ? WHERE uuid = ?");
-                    playerStatement.setObject(1, null);
-                    playerStatement.setString(2, player.getUniqueId().toString());
-
-                    if (playerStatement.executeUpdate() > 0) {
+                    if (removePlayerFromTeam(player.getUniqueId().toString())) {
                         player.sendMessage("[CTF] You have left the " + color + " team, farewell!");
                     }
                 }
-
-
             }
 
         } catch (SQLException e) {
             e.getStackTrace();
         }
+    }
+
+    private boolean removePlayerFromTeam(String uuid) {
+
+        try {
+            PreparedStatement playerStatement = connection.prepareStatement(
+                    "UPDATE users SET team = ? WHERE uuid = ?");
+            playerStatement.setObject(1, null);
+            playerStatement.setString(2, uuid);
+
+            if (playerStatement.executeUpdate() > 0) {
+
+                return true;
+            }
+
+        } catch (SQLException e) {
+            e.getStackTrace();
+        }
+
+        return false;
     }
 
 }
